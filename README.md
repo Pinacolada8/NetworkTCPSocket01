80e401aed74611cfeb423de9a11c19ec90869dc0
Pinacolada <mdcmatheusmdc@gmail.com>
2021-01-20 17:51:56 -0300

    Adding console logs to server and example image

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # NetworkTCPSocket01
-Basic usage of network TCP socket in C#.
+Basic usage of network TCP socket in C# .Net Core 3.1.
+
+# Examples
+- Example 01:
+ ![](images/example01.png)
+    - server ip: 192.168.1.100
+    - client ip: 192.168.1.103
+
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using SDK;
@@ -18,12 +19,24 @@ namespace Server {
                 var inFromClient = new BinaryReader(stream);
                 var outToClient = new BinaryWriter(stream);
 
+                Console.WriteLine("Connected");
+
                 var clientSentence = inFromClient.ReadString();
 
+                Console.WriteLine("Received From Client: " + clientSentence);
+
                 var capitalizedSentence = clientSentence.ToUpper();
+
+                Console.WriteLine("Sent to Client: " + capitalizedSentence);
+
                 outToClient.Write(capitalizedSentence);
 
+                Console.WriteLine("Successfully sent");
+
                 connectionSocket.Close();
+                Console.WriteLine();
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine();
             }
         }
     }
